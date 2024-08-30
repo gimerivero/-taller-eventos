2410944175b98e5c6a2be5f3381dbb2a0c79edcf
gimenarivero <gimerivero24@gmail.com>
2024-08-30 19:20:43 -0300

    alerta agregada

---
+++ eventos.js
@@ -0,0 +1,14 @@
+
+
+        function myFunction() {
+          document.getElementById("saludo").innerHTML = "Hola!";
+        }
+       
+
+const button = document.querySelector("button");
+
+function action() {
+  alert("Hola soy el div");
+};
+
+button.addEventListener("click", action);
